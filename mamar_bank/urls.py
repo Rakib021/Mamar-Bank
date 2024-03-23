b24cf14b1bbc47fef63b7cb4f145f7b72f63562d
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-23 19:42:57 +0600

    admin panel

--- mamar_bank/urls.py
+++ mamar_bank/urls.py
@@ -2,6 +2,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from core.views import HomeView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+
 
 urlpatterns = [
     path('', HomeView.as_view(),name= 'home'),
@@ -9,3 +11,4 @@ urlpatterns = [
     path('accounts/',include('accounts.urls')),
     path('transactions/',include('transaction.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns() # new
